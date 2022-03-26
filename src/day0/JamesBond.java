dac9d6343b3f8bf204304114076bc6a8be811445
mahirtaghizade <mahir95.x@icloud.com>
2022-03-26 16:33:42 -0400

    Edited

--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -1,6 +1,8 @@
-packageday0;
+package day0;
+
 public class JamesBond{
-  public static void main(String[] args({
-    System.out.println("James Bond hacked our project");
-  }
- }
+
+    public static void main(String [] args){
+        System.out.println("James Bond hacked the project");
+    }
+}
